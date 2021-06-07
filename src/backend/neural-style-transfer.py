9534881233bf2318b504e15b8d42cbf37985322e
unknown <jenslorden@gmail.com>
2021-06-07 19:40:38 +0200

    code clean up

---
+++ src/backend/neural-style-transfer.py
@@ -0,0 +1,103 @@
+import tensorflow as tf
+
+content_layers = ['block5_conv2'] 
+
+style_layers = ['block1_conv1',
+                'block2_conv1',
+                'block3_conv1', 
+                'block4_conv1', 
+                'block5_conv1']
+
+num_content_layers = len(content_layers)
+num_style_layers = len(style_layers)
+
+def vggLayers(layer_names):
+    """ Creates a vgg model that returns a list of intermediate output values."""
+    # Load our model. Load pretrained VGG, trained on imagenet data
+    vgg = tf.keras.applications.VGG19(include_top=False, weights='imagenet')
+    vgg.trainable = False
+
+    outputs = [vgg.get_layer(name).output for name in layer_names]
+
+    model = tf.keras.Model([vgg.input], outputs)
+    return model
+
+def gram_matrix(input_tensor):
+    result = tf.linalg.einsum('bijc,bijd->bcd', input_tensor, input_tensor)
+    input_shape = tf.shape(input_tensor)
+    num_locations = tf.cast(input_shape[1]*input_shape[2], tf.float32)
+    return result/(num_locations)
+
+class StyleContentModel(tf.keras.models.Model):
+    def __init__(self, style_layers, content_layers):
+        super(StyleContentModel, self).__init__()
+        self.vgg = vggLayers(style_layers + content_layers)
+        self.style_layers = style_layers
+        self.content_layers = content_layers
+        self.num_style_layers = len(style_layers)
+        self.vgg.trainable = False
+
+    def call(self, inputs):
+        # Run vgg19 on the input
+        # Expects float input in [0,1]
+        inputs = inputs * 255.0
+        preprocessed_input = tf.keras.applications.vgg19.preprocess_input(inputs)
+        outputs = self.vgg(preprocessed_input)
+
+        style_outputs, content_outputs = (outputs[:self.num_style_layers], outputs[self.num_style_layers:])
+        style_outputs = [gram_matrix(style_output) for style_output in style_outputs]
+
+        content_dict = {content_name:value for content_name, value in zip(self.content_layers, content_outputs)}
+        style_dict = {style_name:value for style_name, value in zip(self.style_layers, style_outputs)}
+        return {'content':content_dict, 'style':style_dict}
+
+def clip_0_1(image):
+    return tf.clip_by_value(image, clip_value_min=0.0, clip_value_max=1.0)
+
+def style_content_loss(outputs, style_targets, style_weight, content_targets, content_weight):
+    style_outputs = outputs['style']
+    content_outputs = outputs['content']
+
+    style_loss = tf.add_n([tf.reduce_mean((style_outputs[name]-style_targets[name])**2) 
+                            for name in style_outputs.keys()])
+    style_loss *= style_weight / num_style_layers
+
+    content_loss = tf.add_n([tf.reduce_mean((content_outputs[name]-content_targets[name])**2) 
+                                for name in content_outputs.keys()])
+
+    content_loss *= content_weight / num_content_layers
+    loss = style_loss + content_loss
+    return loss
+
+@tf.function()
+def train_step(image, optimizer, extractor, style_targets, style_weight, content_targets, content_weight):
+    with tf.GradientTape() as tape:
+        outputs = extractor(image)
+        loss = style_content_loss(outputs, style_targets, style_weight, content_targets, content_weight)
+
+        grad = tape.gradient(loss, image)
+        optimizer.apply_gradients([(grad, image)])
+        image.assign(clip_0_1(image))
+
+def trainModel(extractor, style_image, content_image):
+    style_targets = extractor(style_image)['style']
+    content_targets = extractor(content_image)['content']
+    image = tf.Variable(content_image)
+
+    optimizer = tf.optimizers.Adam(learning_rate=0.02, beta_1=0.99, epsilon=1e-1)
+    style_weight=1e-2
+    content_weight=1e4
+
+
+def styleTransfer(style_image, content_image):
+    style_extractor = vggLayers(style_layers)
+    style_outputs = style_extractor(style_image*255)
+
+    extractor = StyleContentModel(style_layers, content_layers)
+    results = extractor(tf.constant(content_image))
+
+
+    return 0
+
+if __name__ == '__main__':
+    styleTransfer()
